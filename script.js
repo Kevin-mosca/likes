1760d26077b2e478a5f1d952ba1d4e2b587e16fa
Kevin-mosca <Kevinmoscaa@gmail.com>
2021-04-27 15:16:30 -0700

    Lets GOOOO

---
+++ script.js
@@ -0,0 +1,21 @@
+var countA =0;
+var countElement = document.querySelector("#countA")
+function adda(){
+    countA++;
+    countElement.innerText = +countA;
+    console.log(countA);
+}
+var countB=0;
+var countElementB = document.querySelector("#countB")
+function addb(){
+    countB++;
+    countElementB.innerText = +countB;
+    console.log(countB)
+}
+var countC=0;
+var countElementC = document.querySelector("#countC")
+function addc(){
+    countC++;
+    countElementC.innerText = +countC;
+    console.log(countC)
+}
